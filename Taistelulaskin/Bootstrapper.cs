2c0e8149492b0f99ef193ef4d9157fe9afb19108
ToivomieliS <44163866+ToivomieliS@users.noreply.github.com>
2020-05-06 10:07:51 +0300

    Itsenäine projekti aloitettu
    Caliburn Micro asennettu solutioniin.
    
    Työ aloitettu MVVM-mallin rakentamiseksi.

---
+++ Taistelulaskin/Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows
+using Caliburn.Micro;
+using Taistelulaskin.ViewModels;
+
+namespace Taistelulaskin
+{
+    public class BootStrapper : BootstrapperBase
+    {
+        public BootStrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
